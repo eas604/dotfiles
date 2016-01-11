ff8c8ea4ac50e434495c6258d5743ba54cbdf39a
Edwin Sheldon <edwin.sheldon@gmail.com>
2016-01-10 20:02:28 -0600

    Add script to symlink to dotfiles

---
+++ make_symlinks.py
@@ -0,0 +1,20 @@
+"""
+Create symlinks for dotfiles if they don't already exist.
+"""
+import os
+
+d = os.path.dirname(os.path.abspath(__file__))
+home = os.path.expanduser('~')
+
+for f in os.listdir(d):
+    if f.startswith('.'):
+        home_f = '{0}/{1}'.format(home, f)
+        if not os.path.exists(home_f):
+            dot_f = '{0}/{1}'.format(d, f)
+            try:
+                os.symlink(src=dot_f, dst=home_f)
+                print('{0} => {1}'.format(dot_f, home_f))
+            except Exception as e:
+                print('Failed to create symlink for {0}'.format(f))
+                print(e)
+                raise e
